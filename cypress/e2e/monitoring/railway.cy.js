fc2dd0a48a827d578510c2a45c0cec3c0a706860
Samir <sobiroff23@gmail.com>
2026-02-13 17:30:13 +0500

    fix

--- cypress/e2e/monitoring/railway.cy.js
+++ cypress/e2e/monitoring/railway.cy.js
@@ -1,51 +1,68 @@
-// describe('Railway Product', () => {
-//   it('Search Flow', () => {
-//     cy.viewport(1280, 800);
+describe('Railway Product', () => {
+  it('Search Flow', () => {
+    cy.viewport(1280, 800);
+    cy.intercept('POST', '**/railway/offers**').as('railSearch');
+
+    // 1. АВТОРИЗАЦИЯ (Твой рабочий XPath)
+    cy.visit('https://test.globaltravel.space/sign-in');
+
+    cy.xpath("(//input[contains(@class,'input')])[1]", { timeout: 20000 })
+      .should('be.visible')
+      .type(Cypress.env('LOGIN_EMAIL'), { log: false });
+
+    cy.xpath("(//input[contains(@class,'input')])[2]")
+      .should('be.visible')
+      .type(Cypress.env('LOGIN_PASSWORD'), { log: false })
+      .type('{enter}');
+
+    cy.url({ timeout: 40000 }).should('include', '/home');
+
+    // 2. ПЕРЕХОД В ЖД
+    cy.visit('https://test.globaltravel.space/railway');
+
+    // 3. ОТКУДА (Ввод + Клик по подсказке)
+    cy.get('input[placeholder="Откуда"]', { timeout: 15000 })
+      .should('be.visible')
+      .click({ force: true })
+      .type('Ташкент', { delay: 150 });
     
-//     // 1. ПЕРЕХВАТ API (замените URL на тот, который вызывается при поиске ЖД)
-//     cy.intercept('POST', '**/railway/offers**').as('railSearch');
-
-//     cy.visit('https://test.globaltravel.space/railway');
-
-//     // 2. ЛОГИН (используем вашу рабочую логику через XPath)
-//     cy.xpath("(//input[contains(@class,'input')])[1]").should('be.visible')
-//       .type(Cypress.env('LOGIN_EMAIL'), { log: false });
-//     cy.xpath("(//input[contains(@class,'input')])[2]")
-//       .type(Cypress.env('LOGIN_PASSWORD'), { log: false }).type('{enter}');
-
-//     cy.url({ timeout: 40000 }).should('include', '/railway');
-
-//     // 3. ОТКУДА
-//     cy.get('#from').click({force: true}).clear().type('Ташкент', { delay: 200 });
-//     cy.wait(1000);
-//     cy.get('#from').type('{enter}');
+    // Кликаем по элементу списка p-listbox-item (как на твоем скрине)
+    cy.get('li.p-listbox-item', { timeout: 10000 })
+      .contains('ТАШКЕНТ')
+      .should('be.visible')
+      .click({ force: true });
     
-//     cy.wait(1000);
+    // 4. КУДА (Ввод + Клик по подсказке)
+    cy.get('input[placeholder="Куда"]')
+      .should('be.visible')
+      .click({ force: true })
+      .type('Самарканд', { delay: 150 });
 
-//     // 4. КУДА
-//     cy.get('#to').click({force: true}).clear().type('Самарканд', { delay: 250 });
-//     cy.wait(1500); 
-//     cy.get('#to').type('{enter}');
-//     cy.wait(1000);
+    cy.get('li.p-listbox-item', { timeout: 10000 })
+      .contains('САМАРКАНД')
+      .should('be.visible')
+      .click({ force: true });
 
-//     // 5. ДАТА
-//     cy.get("input[placeholder='Когда']").click();
-//     const targetDay = new Date();
-//     targetDay.setDate(targetDay.getDate() + 2); // Дата через 2 дня
-//     const dayToSelect = targetDay.getDate();
+    // 5. ДАТА
+    cy.get("input[placeholder='Когда']").click();
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 2);
+    const day = targetDate.getDate();
 
-//     cy.get('.p-datepicker-calendar td').not('.p-datepicker-other-month')
-//       .contains(new RegExp(`^${dayToSelect}$`)).click({ force: true });
+    cy.get('.p-datepicker-calendar td').not('.p-datepicker-other-month')
+      .contains(new RegExp(`^${day}$`))
+      .click({ force: true });
 
-//     cy.get('body').type('{esc}');
-//     cy.wait(2000); 
+    cy.get('body').type('{esc}');
 
-//     // 6. ПОИСК
-//     cy.get('#search-btn').should('be.visible').click({ force: true });
+    // 6. ПОИСК (Используем класс easy-button со скриншота)
+cy.get('button.easy-button').filter(':visible').last()
+      .should('be.visible')
+      .click({ force: true });
 
-//     // 7. ОЖИДАНИЕ ОТВЕТА API И ПРОВЕРКА 200
-//     cy.wait('@railSearch', { timeout: 60000 }).then((interception) => {
-//       expect(interception.response.statusCode).to.eq(200);
-//     });
-//   });
-// });
+    // 7. API ПРОВЕРКА
+    cy.wait('@railSearch', { timeout: 60000 }).then((xhr) => {
+      expect(xhr.response.statusCode).to.eq(200);
+    });
+  });
+});
