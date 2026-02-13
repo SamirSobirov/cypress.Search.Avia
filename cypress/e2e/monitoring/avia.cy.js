353f43aacacf2deb19a6583b156b6fabd4b83be0
Samir <sobiroff23@gmail.com>
2026-02-13 16:08:05 +0500

    fix

---
+++ cypress/e2e/monitoring/avia.cy.js
@@ -0,0 +1,52 @@
+describe('Avia Product', () => {
+  it('Search Flow', () => {
+    cy.viewport(1280, 800);
+    cy.intercept('POST', '**/offers**').as('apiSearch');
+
+    cy.visit('https://test.globaltravel.space/home');
+
+    // 1. ЛОГИН (ваш XPath)
+    cy.xpath("(//input[contains(@class,'input')])[1]").should('be.visible')
+      .type(Cypress.env('LOGIN_EMAIL'), { log: false });
+    cy.xpath("(//input[contains(@class,'input')])[2]")
+      .type(Cypress.env('LOGIN_PASSWORD'), { log: false }).type('{enter}');
+
+    cy.url({ timeout: 40000 }).should('include', '/home');
+
+    // 2. ОТКУДА
+    cy.get('#from').click({force: true}).clear().type('Ташкент', { delay: 200 });
+    cy.wait(1000);
+    cy.get('#from').type('{enter}');
+    
+    cy.wait(1000);
+
+    // 3. КУДА
+    cy.get('#to').click({force: true}).clear().type('Москва', { delay: 250 });
+    cy.wait(1500); 
+    cy.get('#to').type('{enter}');
+    cy.wait(1000);
+
+    // 4. ДАТА (Улучшенный поиск числа)
+    cy.get("input[placeholder='Когда']").click();
+    
+    const targetDay = new Date();
+    targetDay.setDate(targetDay.getDate() + 2);
+    const dayToSelect = targetDay.getDate();
+
+    // Добавляем проверку, что календарь появился, и ищем именно текст внутри ячеек
+    cy.get('.p-datepicker-calendar td').not('.p-datepicker-other-month')
+      .contains(new RegExp(`^${dayToSelect}$`))
+      .click({ force: true });
+
+    cy.get('body').type('{esc}');
+    cy.wait(2000); 
+
+    // 5. ПОИСК
+    cy.get('#search-btn').should('be.visible').click({ force: true });
+
+    // 6. ПРОВЕРКА РЕЗУЛЬТАТА (API)
+    cy.wait('@apiSearch', { timeout: 60000 }).then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+    });
+  });
+});
